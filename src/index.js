9698b31c1e4ee37320dd986b2bede9575f88458a
gokuthecoder <vishalkr12012001@gmail.com>
2024-03-09 22:19:57 +0530

    package.json and index.js updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { app } = require("./app.js")
 const {connectDB} = require("./db/index.js")
 const auth = require('./file/auth.forward.js')
+const express = require("express")
+const app = express()
 const dotenv = require("dotenv")
 dotenv.config()
 
@@ -10,4 +12,12 @@ connectDB()
     })
     .catch((err) => {
         console.log('MONGO db connection failed !!!', err);
+    })
+
+    app.get("/",(req,res)=>{
+        res.status(200).json({
+            name:"vishal",
+            class:12,
+            roll:"2ADDHDH&^&"
+        })
     })
